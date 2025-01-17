74140022dd19a72628f5e9efb835190b9f4b1770
CharlesGBraga <charlesbraga60@gmail.com>
2025-01-17 13:21:36 -0300

    adjusting wp_config.php

--- wp-config.php
+++ wp-config.php
@@ -44,20 +44,6 @@ define('NONCE_SALT',       'iUjXfD~]JYZlmuGx%}+u63A@TXCTv74:mwD?)kF3S1!)Sh8_=+gO
 // Definições do WordPress (geralmente não precisam ser alteradas)
 define('WP_DEBUG', false);
 
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
- 
-/**#@-*/
-
 /**
  * WordPress database table prefix.
  *
@@ -71,20 +57,6 @@ define('WP_DEBUG', false);
  * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
 $table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
- */
-define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
